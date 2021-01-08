4b6ac329d8ad2d150240bc3a49974c529ed2e060
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-01-08 16:48:44 +0000

    licence header, default port, stub connection config

--- app/uk/gov/hmrc/vatdeferralnewpaymentscheme/controllers/DirectDebitArrangementController.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentscheme/controllers/DirectDebitArrangementController.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2021 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.vatdeferralnewpaymentscheme.controllers
@@ -87,6 +98,7 @@ class DirectDebitArrangementController @Inject()(
           true,
           dd.toList)
 
+        // TODO fix: this fails silently e.g. if the VAT Number is unknown
         for {
           c <- vatRegisteredCompaniesConnector.lookup(vrn)
           letterAndControl = LetterAndControl(
