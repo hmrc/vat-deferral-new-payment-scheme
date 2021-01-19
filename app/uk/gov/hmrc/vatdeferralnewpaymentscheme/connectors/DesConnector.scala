1e7b30f8ce050d3ddfe060eefd3db7ee7aab994d
Paul Thorrington <3472481+PaulThor@users.noreply.github.com>
2021-01-19 16:40:11 +0000

    Reverted copyright

--- app/uk/gov/hmrc/vatdeferralnewpaymentscheme/connectors/DesConnector.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentscheme/connectors/DesConnector.scala
@@ -1,12 +1,22 @@
 /*
  * Copyright 2021 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.vatdeferralnewpaymentscheme.connectors
 
 import javax.inject.Inject
-import play.api.Logger
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, NotFoundException}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.vatdeferralnewpaymentscheme.config.AppConfig
@@ -28,7 +38,6 @@ class DesConnector @Inject() (http: HttpClient, servicesConfig: ServicesConfig, 
 
   def getObligations(vrn: String) = {
     val url: String = s"$serviceURL/${appConfig.getObligationsPath.replace("$vrn", vrn)}"
-    Logger.logger.debug(s"url: $url")
     http.GET[ObligationData](url) recover {
       case _: NotFoundException => ObligationData(List.empty[Obligations])
     }
