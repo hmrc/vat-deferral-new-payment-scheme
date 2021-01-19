05b3f8f6769c1433233efa88b5194691a09554a9
Paul Thorrington <3472481+PaulThor@users.noreply.github.com>
2021-01-19 15:55:37 +0000

    Made the get obligations and financial apis configation settings@

--- app/uk/gov/hmrc/vatdeferralnewpaymentscheme/connectors/DesConnector.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentscheme/connectors/DesConnector.scala
@@ -1,30 +1,21 @@
 /*
  * Copyright 2021 HM Revenue & Customs
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package uk.gov.hmrc.vatdeferralnewpaymentscheme.connectors
 
 import javax.inject.Inject
+import play.api.Logger
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, NotFoundException}
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import uk.gov.hmrc.vatdeferralnewpaymentscheme.config.AppConfig
 import uk.gov.hmrc.vatdeferralnewpaymentscheme.model.financialdata.FinancialData
 import uk.gov.hmrc.vatdeferralnewpaymentscheme.model.obligations.{ObligationData, Obligations}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class DesConnector @Inject() (http: HttpClient, servicesConfig: ServicesConfig) {
+class DesConnector @Inject() (http: HttpClient, servicesConfig: ServicesConfig, appConfig: AppConfig) {
 
   private def getConfig(key: String) = servicesConfig.getConfString(key, "")
 
@@ -36,14 +27,15 @@ class DesConnector @Inject() (http: HttpClient, servicesConfig: ServicesConfig) 
   implicit val headerCarrier = HeaderCarrier(extraHeaders = headers)
 
   def getObligations(vrn: String) = {
-    val url: String = s"${serviceURL}/enterprise/obligation-data/vrn/$vrn/VATC?from=2016-04-06&to=2020-04-06&status=O"
+    val url: String = s"$serviceURL/${appConfig.getObligationsPath.replace("$vrn", vrn)}"
+    Logger.logger.debug(s"url: $url")
     http.GET[ObligationData](url) recover {
       case _: NotFoundException => ObligationData(List.empty[Obligations])
     }
   }
 
   def getFinancialData(vrn: String)= {
-    val url: String = s"${serviceURL}/enterprise/financial-data/VRN/$vrn/VATC?dateFrom=2020-03-20&dateTo=2020-06-30"
+    val url: String = s"$serviceURL/${appConfig.getFinancialDataPath.replace("$vrn", vrn)}"
     http.GET[FinancialData](url)
   }
 }
