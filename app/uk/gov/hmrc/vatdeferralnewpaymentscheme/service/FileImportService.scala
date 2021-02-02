bf8469a56b04810b29a704fb89cb47821d37893e
paulthor <3472481+PaulThorrington@users.noreply.github.com>
2021-02-02 15:41:07 +0000

    Development testing

--- app/uk/gov/hmrc/vatdeferralnewpaymentscheme/service/FileImportService.scala
+++ app/uk/gov/hmrc/vatdeferralnewpaymentscheme/service/FileImportService.scala
@@ -16,6 +16,9 @@
 
 package uk.gov.hmrc.vatdeferralnewpaymentscheme.service
 
+import com.amazonaws.services.s3.model.GetObjectTaggingRequest
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+
 import javax.inject.Inject
 import play.api.Logger
 import play.api.Logger.logger
@@ -26,46 +29,62 @@ import uk.gov.hmrc.vatdeferralnewpaymentscheme.repo.{ImportFileRepo, PaymentOnAc
 
 import scala.concurrent.ExecutionContext
 
-class FileImportService @Inject()(
-  amazonS3Connector: AmazonS3Connector,
-  timeToPayRepo: TimeToPayRepo,
-  paymentOnAccountRepo: PaymentOnAccountRepo,
-  fileImportRepo: ImportFileRepo,
-  config: AppConfig
-)(implicit ec: ExecutionContext) {
+class FileImportService @Inject()(config: AppConfig)(implicit ec: ExecutionContext) {
 
   def importS3File(): Unit = {
     //importFile(config.ttpFilename, { fc => timeToPayRepo.addMany(ParseTTPFile(fc)) })
+
     logger.debug(s"importS3File triggered with parameters filename:${config.ttpFilename}, region:${config.region}, bucket:${config.bucket}")
-    logger.debug(s"Get object: ${amazonS3Connector.getObject(config.ttpFilename)}")
-  }
 
-  private def importFile(filename: String, updateCollection: (String) => Unit) : Unit = {
-
-    if (amazonS3Connector.exists(filename)) {
-      val s3FileLastModifiedDate = amazonS3Connector.getObject(filename).getObjectMetadata.getLastModified
-
-      fileImportRepo.lastModifiedDate(filename).map {
-        case Some(date) if !s3FileLastModifiedDate.after(date) =>
-          Logger.logger.debug(s"filename:$filename is up to date s3:${s3FileLastModifiedDate} is after: ${s3FileLastModifiedDate.after(date)} ")
-        case _ => {
-          val contentBytes = amazonS3Connector.objectContentBytes(filename)
-          val fileContents = contentBytes.map(_.toChar).mkString
-          updateCollection(fileContents)
-          fileImportRepo.updateLastModifiedDate(filename, s3FileLastModifiedDate)
-        }
-      }
+    val s3client: AmazonS3 = {
+      val builder = AmazonS3ClientBuilder
+        .standard()
+        .withPathStyleAccessEnabled(true)
+      builder.withRegion(config.region)
+      builder.build()
     }
-    else logger.debug(s"File does not exist: $filename")
-  }
 
-  private def ParseTTPFile(fileContents: String): Array[TimeToPay] = {
-    fileContents.split('\n').drop(1).dropRight(1).map {
-      line => {
-        val lineSplit = line.split(',')
-        val vrn: String = lineSplit(1).replace("\r", "")
-        TimeToPay(vrn)
+    try {
+      val response = s3client.getObject(config.bucket, config.ttpFilename)
+      logger.debug(s"S3 Get Object ${response}")
+    } catch {
+      case e: Exception => {
+        logger.error(s"S3 Exception: ${e}")
       }
     }
   }
+
+
+
+
+  //    logger.debug(s"Get object: ${amazonS3Connector.getObject(config.ttpFilename)}")
+
+//  private def importFile(filename: String, updateCollection: (String) => Unit) : Unit = {
+//
+//    if (amazonS3Connector.exists(filename)) {
+//      val s3FileLastModifiedDate = amazonS3Connector.getObject(filename).getObjectMetadata.getLastModified
+//
+//      fileImportRepo.lastModifiedDate(filename).map {
+//        case Some(date) if !s3FileLastModifiedDate.after(date) =>
+//          Logger.logger.debug(s"filename:$filename is up to date s3:${s3FileLastModifiedDate} is after: ${s3FileLastModifiedDate.after(date)} ")
+//        case _ => {
+//          val contentBytes = amazonS3Connector.objectContentBytes(filename)
+//          val fileContents = contentBytes.map(_.toChar).mkString
+//          updateCollection(fileContents)
+//          fileImportRepo.updateLastModifiedDate(filename, s3FileLastModifiedDate)
+//        }
+//      }
+//    }
+//    else logger.debug(s"File does not exist: $filename")
+//  }
+//
+//  private def ParseTTPFile(fileContents: String): Array[TimeToPay] = {
+//    fileContents.split('\n').drop(1).dropRight(1).map {
+//      line => {
+//        val lineSplit = line.split(',')
+//        val vrn: String = lineSplit(1).replace("\r", "")
+//        TimeToPay(vrn)
+//      }
+//    }
+//  }
 }
