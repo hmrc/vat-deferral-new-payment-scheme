f7fa9bb0139003a8d5e2365292b81c8b44ef4f7c
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-02-04 21:56:35 +0000

    library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.0.0",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"       % "7.30.0-play-27",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.4.0",
+    "uk.gov.hmrc"             %% "simple-reactivemongo"       % "7.31.0-play-27",
     "com.amazonaws"           % "aws-java-sdk-s3"             % "1.11.915",
     "uk.gov.hmrc"             %% "stub-data-generator"        % "0.5.3",
     "org.typelevel"           %% "cats-core"                  % "2.1.1"
